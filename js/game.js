ff566a53ddc0f801f4b4f3067cf8a04264659128
にゃるどびす <tragoidi4@gmail.com>
2026-09-26 00:53:24 +0900

    不要な表向きにする操作を削除

--- js/game.js
+++ js/game.js
@@ -118,9 +118,7 @@ function selection(){
   }
   op.innerHTML="";
   if(s.p!==1){op.textContent="相手のカードは確認のみ";return}
-  if(c.faceUp===false){
-    add("表向きにする",()=>{c.faceUp=true;render()});
-  }else if(s.z!=="monsters"){
+  if(c.faceUp!==false&&s.z!=="monsters"){
     add("裏向きにする",()=>{c.faceUp=false;state.selected=null;render()});
   }
   if(s.z==="monsters"||s.z==="energy"){add("タップ / アンタップ",()=>{c.tapped=!c.tapped;state.selected={p:s.p,z:s.z,id:s.id};render()});}
